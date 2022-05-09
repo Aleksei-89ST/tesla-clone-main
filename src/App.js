2771c1e3cf69e21d7a5a825a85c9ea5bedf02208
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-09 23:02:24 +0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from "react";
-import { Footer } from "./componenets/Footer/Footer";
-import { Header } from "./componenets/Header/Header";
-import { Home } from "./componenets/Home/Home";
+import React, { useEffect } from "react";
+import { Home } from "./components/Home/Home";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Car } from "./components/Car/Car";
+import { Layout } from "./components/Layout/Layout";
+import carInfo from "./carInfo.json";
+import { SolarRoof } from "./components/SolarRoof/SolarRoof";
+import { SolarPanels } from "./components/SolarPanels/SolarPanels";
+import { Login } from "./components/Login/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./store/userSlise";
+import { CreateAccount } from "./components/Login/CreateAccount";
+import { TeslaAccount } from "./components/TeslaAccount/TeslaAccount";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(login(user));
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <Footer />
-      <Header />
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        {carInfo.map((item) => (
+          <Route path={item.path} key={item.id} element={<Car {...item} />} />
+        ))}
+        <Route path="/solarroof" element={<SolarRoof />} />
+        <Route path="/solarpanels" element={<SolarPanels />} />
+        <Route path="/teslaaccount" element={<TeslaAccount />} />
+      </Route>
+      <Route
+        path="/login"
+        element={user ? <Navigate to="/teslaaccount" /> : <Login />}
+      />
+      <Route
+        path="/createaccount"
+        element={user ? <Navigate to="/teslaaccount" /> : <CreateAccount />}
+      />
+    </Routes>
   );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "modern-normalize";
+import { GlobalStyle } from "./GlobalStyle";
+import { Provider } from "react-redux";
+import { store } from "./store/store"
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <GlobalStyle />
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
